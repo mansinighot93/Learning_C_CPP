0e5d05db88572bc9704d2809f2cdebc753349f2f
mansinighot93 <mansinighot93@gmail.com>
2024-11-02 20:48:55 +0530

    changes

---
+++ C++/BookStack.cpp
@@ -0,0 +1,63 @@
+//Implementation of Class
+#include<stdio.h>           //Header files // System
+#include <iostream> 
+#include "./Header_Files/BookStack.h"        //System.IO
+
+using namespace std;
+
+BookStack::BookStack()
+{
+    top = BOTTOM;
+}
+
+bool BookStack::IsFull()
+{
+    bool status = false;
+    if (top == MAX - 1)
+    {
+        status = true;
+    }
+     return status;
+}
+
+bool BookStack::IsEmpty()
+{
+    bool status = false;
+    if (top == BOTTOM)
+    {
+        status = true;
+    }
+    return status;
+}
+
+void BookStack::Push(int bookId)
+{
+    top = top + 1;
+    books[top] = bookId;
+}
+
+int BookStack::Pop()
+{
+    int bookIdTop = 0;
+    if (!IsEmpty()) {
+         bookIdTop = books[top];
+        top = top - 1;
+    }
+    return bookIdTop;
+}
+
+void BookStack::Print()
+{
+    if (!IsEmpty())
+    {
+        cout << "\n Printing all Book Ids:  ";
+        for (int i = top; i >= 0; i--)
+        {
+            cout << "\n" << books[i];
+        } 
+    }
+    else
+    {
+        cout << "\n Book Stack is empty";
+    }
+}
